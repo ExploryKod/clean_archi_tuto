6d96d859698bcbffd0124a6f68fd3034b74744bb
Amaury Franssen <amauryfranssen@gmail.com>
2023-09-13 22:20:26 +0200

    feat: install tailwind postcss and create a jumbotron

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { OrderPage } from "@ratatouille/modules/order/react/pages/order/OrderPage";
-
+import "@ratatouille/app/app.css";
 export default function Home() {
   return <OrderPage />;
 }
--- src/modules/order/react/pages/order/OrderPage.tsx
+++ src/modules/order/react/pages/order/OrderPage.tsx
@@ -1,7 +1,9 @@
 "use client";
-
 import React from "react";
 
+
 export const OrderPage: React.FC = () => {
-  return <main></main>;
+  return <main>
+    
+  </main>;
 };
