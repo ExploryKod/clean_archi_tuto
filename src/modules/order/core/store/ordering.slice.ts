2cb3fdd0dd0e48d269b31139977724c3af63997b
Amaury Franssen <amauryfranssen@gmail.com>
2024-01-05 19:46:49 +0100

    feat(steps): make components for steps and one test for table transition

--- src/modules/order/core/store/ordering.slice.ts
+++ src/modules/order/core/store/ordering.slice.ts
@@ -1,11 +1,21 @@
 import { OrderingDomainModel } from "@ratatouille/modules/order/core/model/ordering.domain-model";
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
+export enum orderingStep {
+    GUESTS = 0,
+    TABLE = 5,
+    MEALS = 3,
+    SUMMARY = 2,
+    RESERVED = 4
+}
+
 export type OrderingState = {
+    step: orderingStep,
     form: OrderingDomainModel.Form
 }
 
 export const initialState: OrderingState = {
+    step: orderingStep.GUESTS,
     form: {
         guests: [],
         organizerId: null
@@ -18,6 +28,7 @@ export const orderingSlice = createSlice({
     reducers: {
         chooseGuests(state, action:PayloadAction<OrderingDomainModel.Form>){
             state.form = action.payload;
+            state.step = orderingStep.TABLE;
         } 
     }
 });
--- src/modules/order/react/pages/order/OrderPage.tsx
+++ src/modules/order/react/pages/order/OrderPage.tsx
@@ -1,15 +1,24 @@
 "use client";
 import React from "react";
-import { GuestSection } from "@ratatouille/modules/order/react/sections/GuestSection";
-import { RestaurantSection } from "@ratatouille/modules/order/react/sections/RestaurantSection";
-import { HeroSection } from "@ratatouille/modules/order/react/sections/HeroSection";
+import { GuestSection } from "@ratatouille/modules/order/react/sections/guest/GuestSection";
+import { RestaurantSection } from "@ratatouille/modules/order/react/sections/restaurant/RestaurantSection";
+import { HeroSection } from "@ratatouille/modules/order/react/sections/hero/HeroSection";
 import { useOrderPage } from "@ratatouille/modules/order/react/pages/order/use-order-page";
+import { AppState } from "@ratatouille/modules/store/store";
+import { orderingStep } from "@ratatouille/modules/order/core/store/ordering.slice";
+import { MealsSection } from "@ratatouille/modules/order/react/sections/meals/MealsSection";
+import { TableSection } from "@ratatouille/modules/order/react/sections/table/TableSection";
+import { SummarySection } from "@ratatouille/modules/order/react/sections/summary/summarySection";
+import { ReservedSection } from "@ratatouille/modules/order/react/sections/reserved/reservedSection";
+import { useSelector } from "react-redux";
+
 
 export const OrderPage: React.FC = () => {
   const presenter = useOrderPage();
-
+  const step = useSelector((state: AppState) => state.ordering.step);
 // attention les props ici de showGuestSection rendent sûrement le composant non réutilisable > pas clean archi
 // Solution ? 
+
   return <main className="flex flex-col">
       <HeroSection showGuestSection={presenter.showGuestSection} />
 
@@ -17,7 +26,13 @@ export const OrderPage: React.FC = () => {
       (<>
       <div className="pt-5 w-full min-h-[100vh] bg-gradient-to-r from-amber-200 to-yellow-500 flex flex-col gap-10">
         <RestaurantSection restaurantList={presenter.restaurantList} selectRestaurant={presenter.selectRestaurant}/>
-        {!presenter.restaurantList.restaurantId || <GuestSection restaurantList={presenter.restaurantList} />}
+         
+        {!(presenter.restaurantList.restaurantId && step === orderingStep.GUESTS) || <GuestSection restaurantList={presenter.restaurantList} />}
+        {!(presenter.restaurantList.restaurantId && step === orderingStep.MEALS) || <MealsSection />}
+        {!(presenter.restaurantList.restaurantId && step === orderingStep.TABLE) || <TableSection />}
+        {!(presenter.restaurantList.restaurantId && step === orderingStep.SUMMARY) || <SummarySection />}
+        {!(presenter.restaurantList.restaurantId && step === orderingStep.RESERVED) || <ReservedSection />}
+        
       </div>
       <div ref={presenter.bottomRef}></div>
       </>)}
